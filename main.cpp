de485b23a2a72e3cb17ca800dc88090728aa7fce
IguchiShunpei <k021g1129@g.neec.ac.jp>
2022-04-13 17:28:45 +0900

    デスクリプタヒープの生成

--- main.cpp
+++ main.cpp
@@ -169,7 +169,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 		commandQueue, hwnd, &swapChainDesc, nullptr, nullptr,
 		(IDXGISwapChain1**)&swapChain);
 	assert(SUCCEEDED(result));
+	 
+	//デスクリプタヒープの生成
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc{};
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;       //レンダーターゲットビュー
+	rtvHeapDesc.NumDescriptors = swapChainDesc.BufferCount;  //裏表の2つ
 
+	//デスクリプタヒープの生成
+	device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&rtvHeap));
 
 	//DirectX 初期化処理　ここまで
 
